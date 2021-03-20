9b12757c7b36a45810942dcea913cc7363c518a0
Mandy Yip <yipmanyee@hotmail.com>
2021-03-19 21:27:17 -0700

    Carousel

--- src/components/AmenitiesComponent.js
+++ src/components/AmenitiesComponent.js
@@ -1,65 +1,64 @@
 import React from 'react';
 import {ListGroup, ListGroupItem} from 'reactstrap';
 import { Table } from 'reactstrap';
+import ApartImages from './CarouselComponent';
 
 function Amenities(props) {
     return(
-        <React.Fragment>
-        <div className="container">
-            <div className="row">
-            <div className="col">
-            <img src="/assets/images/K_Living Room.jpeg" className="img-fluid" alt="living room"></img>
-            </div>
-            <div className="col">
-            <img src="/assets/images/K_Front Yard.JPG" className="img-fluid" alt="front yard"></img>
-            </div>
-            <div className="col">
-            <img src="/assets/images/K_Kitchen.jpeg" className="img-fluid" alt="kitchen"></img>
-            </div>
-            </div>
-        </div>
-        <div>
-            <div className="row">
-            <div className="col-6">
-            <h2>Bedroom Information</h2>
-                <Table striped>
-                    <thead>
-                        <tr>
-                            <th>Configuration</th>
-                            <th>Bath</th>
-                            <th># of Units</th>
-                            <th>Average Sq Feet</th>
-                        </tr>
-                    </thead>
-                        <tbody>
-                            <tr>
-                                <th scope="row">One Bedroom</th>
-                                <td>1 Bath</td>
-                                <td>4</td>
-                                <td>600 Sq Feet</td>
-                            </tr>
-                            <tr>
-                                <th scope="row">Two Bedroom</th>
-                                <td>2 Bath</td>
-                                <td>6</td>
-                                <td>800 Sq Feet</td>
-                            </tr>
-                        </tbody>
-                </Table>
-            </div>
-        <div className="col-6">        
-            <h2>Amenities</h2>    
-            <ListGroup>
-                <ListGroupItem>1 Parking Stall per unit</ListGroupItem>
-                <ListGroupItem>Utilities included water, sewer and garbage. Tenant pays electrical</ListGroupItem>
-                <ListGroupItem>Secured Laundry Room with coin operated</ListGroupItem>
-                <ListGroupItem>Front and back door entrance</ListGroupItem>
-                <ListGroupItem>Electric Stove / Oven</ListGroupItem>
-                <ListGroupItem>Bathtub</ListGroupItem>
-            </ListGroup>
+        <React.Fragment>            
+            <div className="container">
+                <div className="row">
+                    <div className="col-6">
+                        <ApartImages />
+                    </div>    
+                </div>
+        
+                <div>
+                    <div className="col-6">
+                        <h2>Bedroom Information</h2>
+                            <Table striped>
+                                <thead>
+                                    <tr>
+                                        <th>Configuration</th>
+                                        <th>Bath</th>
+                                        <th># of Units</th>
+                                        <th>Average Sq Feet</th>
+                                    </tr>
+                                </thead>
+                                    <tbody>
+                                        <tr>
+                                            <th scope="row">One Bedroom</th>
+                                            <td>1 Bath</td>
+                                            <td>4</td>
+                                            <td>600 Sq Feet</td>
+                                        </tr>
+                                        <tr>
+                                            <th scope="row">Two Bedroom</th>
+                                            <td>2 Bath</td>
+                                            <td>6</td>
+                                            <td>800 Sq Feet</td>
+                                        </tr>
+                                    </tbody>
+                            </Table>
+                    </div>
+        
+                <div className="row">
+                    <div className="col-6">        
+                        <h2>Amenities</h2>    
+                            <ListGroup>
+                                <ListGroupItem>1 Parking Stall per unit</ListGroupItem>
+                                <ListGroupItem>Utilities included water, sewer and garbage. Tenant pays electrical</ListGroupItem>
+                                <ListGroupItem>Secured Laundry Room with coin operated</ListGroupItem>
+                                <ListGroupItem>Front and back door entrance</ListGroupItem>
+                                <ListGroupItem>Electric Stove / Oven</ListGroupItem>
+                                <ListGroupItem>Bathtub</ListGroupItem>
+                            </ListGroup>
+                        <hr />
+                    </div>
+                </div>
             </div>
         </div>
-        </div>
+        
         </React.Fragment>
     );
 }
--- src/components/CarouselComponent.js
+++ src/components/CarouselComponent.js
@@ -1,31 +1,73 @@
-import { Carousel } from 'bootstrap';
-import React from 'react';
-import { UncontrolledCarousel } from 'reactstrap';
+import { Carousel, CarouselItem, CarouselControl, CarouselIndicators, CarouselCaption } from 'reactstrap';
+import React, {useState} from 'react';
 
 const items = [
     {
-        src: "/assets/images/K_Parking.jpg",
-        altText: 'Slide 1',
-        caption: 'Front Side Parking',
-        header: 'Front Side Parking',
-        key: '1'
+        src: "/assets/images/K_Kitchen.jpeg",
+        altText: "Kitchen",
+        caption: 'cooking range',
+        header: 'Kitchen'
     },
     {
         src: "/assets/images/K_Front Yard.jpg",
-        altText: 'Slide 2',
-        caption: 'Front Yard',
-        header: 'Front Yard',
-        key: '2'
+        altText: 'Front Yard',
+        caption: 'Spacious area',
+        header: 'Front Yard'
     },
     {
         src: "/assets/images/K_Living Room.jpeg",
-        altText: 'Slide 3',
-        caption: 'Living Room',
-        header: 'SLiving Room',
-        key: '3'
+        altText: 'Living Room',
+        caption: 'Heater, carpet',
+        header: 'Living Room',
     },
 ];
 
-const ApartImages = () => <UncontrolledCarousel items={items} />;
+const ApartImages = (props) => {
+    const [activeIndex, setActiveIndex] = useState(0);
+    const [animating, setAnimating] = useState(false);
 
-export default Carousel;
+    const next = () => {
+        if (animating) return;
+        const nextIndex = activeIndex === items.length - 1 ? 0 : activeIndex + 1;
+        setActiveIndex(nextIndex);
+    }
+
+    const previous = () => {
+        if (animating) return;
+        const nextIndex = activeIndex === 0 ? items.length - 1 : activeIndex - 1;
+        setActiveIndex(nextIndex);
+    }
+
+    const goToIndex = (newIndex) => {
+        if (animating) return;
+        setActiveIndex(newIndex);
+    }
+
+    const slides = items.map((item) => {
+        return (
+            <CarouselItem
+            onExiting={() => setAnimating(true)}
+            onExited={() => setAnimating(false)}
+            key={item.src}
+            >
+            <img src={item.src} alt={item.altText} />
+            <CarouselCaption captionText={item.caption} captionHeader={item.header} />
+            </CarouselItem>
+        );
+    });
+
+    return (
+        <Carousel
+        activeIndex={activeIndex}
+        next={next}
+        previous={previous}
+        >
+        <CarouselIndicators items={items} activeIndex={activeIndex} onClickHandler={goToIndex} />
+        {slides}
+        <CarouselControl direction="prev" directionText="Previous" onClickHandler={previous} />
+        <CarouselControl direction="next" directionText="Next" onClickHandler={next} />
+        </Carousel>
+    );
+}
+
+export default ApartImages;
